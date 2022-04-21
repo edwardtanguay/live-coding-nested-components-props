e7488e5cffbefe15dd0285e23a2f952a41c8b834
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-21 10:08:59 +0200

    displaying employees

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.scss'
+import './App.scss';
+import employees from './data/employees.json';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Employee Site</h1>
+			<ul>
+				{employees.map((emp, index) => {
+					return <li>{emp.firstName} {emp.lastName}</li>;
+				})}
+			</ul>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
