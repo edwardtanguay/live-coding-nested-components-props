379cd1a49bc0c78d286bce65777238c7c4f0e4c1
Edward Tanguay (windows) <edward@tanguay.info>
2022-04-21 11:53:46 +0200

    added GeneralStatusMessage component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,14 @@
+import { GeneralStatusMessage } from './GeneralStatusMessage';
+
 export const Header = ({ status }) => {
-
-	let message = '';
-	switch (status) {
-		case 'internalUser':
-			message = 'The summer employee picnic is on May 15.';
-			break;
-		case 'externalUser':
-			message = 'This is our team:';
-			break;
-		default:
-			message = '******* HAVE A NICE DAY ******';
-			break;
-	}
-
 	return (
 		<>
 			<h1>Employee Site</h1>
-			<div className="message">{message}</div>
+			<GeneralStatusMessage
+				status={status}
+				internalText="The summer employee picnic is on May 15."
+				externalText="This is our team:"
+			/>
 		</>
 	);
-}
+};
